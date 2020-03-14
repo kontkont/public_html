893b9add18963ba720d9600d6a745da8d4efcdda
kont88 <kontanistovigor@yandex.ru>
2020-03-14 12:09:36 +0300

    2test

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<?php
-
-//https://www.youtube.com/watch?v=nHjKXiRLAQY
-
-$dvach = array(
-        array("hui", "pizda", "anal", "minsk"),
-        array("dick", "vagina", "dirka", "msk"),
-        array("penis", "shel", "jopa", "piter"));
-echo $dvach[1][2] ;
-$sosach = $dvach[2][3];
-echo  $sosach;
-?>
-
-<br>
 
 <a href="1post.php">Упражнение - представься щенок</a>
 <br>
