90b7cda078925df98f1a748831658db868cd6b88
kont88 <kontanistovigor@yandex.ru>
2020-03-22 15:19:43 +0300

    6

--- 1post.php
+++ 1post.php
@@ -1,9 +1,10 @@
 <form method="POST" action="2sayhello.php">
-Your Name: <input type="text" name="user1" />
-<br>
-Your Surame: <input type="text" name="user2" />
-<br>
-<button type="submit"">Say Hello</button>
+    Your Name: <input type="text" name="user1"/>
+    <br>
+    Your Surame: <input type="text" name="user2"/>
+    <br>
+    <button type="submit"
+    ">Say Hello</button>
 </form>
 
 <br>
